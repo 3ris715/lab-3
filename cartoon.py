baa3225b1e0a7ffa8b233c6c5a4c02d7315ee647
Sara Atluri <sara@atluri.org>
2025-02-07 14:43:23 -0500

    Added convert_image_to_cartoon function, formatting

--- cartoon.py
+++ cartoon.py
@@ -1,5 +1,6 @@
 # missing import statements should be added here
 import wikipedia
+import cv2
 from images import get_wikipedia_page_thumbnail_url, download_image_from_url
 
 def prompt_for_image():
@@ -11,27 +12,42 @@ def prompt_for_image():
     search_query = input("Enter name of a personality: ")
     try:
         results = list(wikipedia.search(search_query, results=3))
-        print("Select a name from the following list:")
+        print("\nSelect a name from the following list:")
         for i in range(len(results)):
             print("{}. {}".format(i+1, results[i]))
         number = int(input("Enter the number of the desired name: "))
-        url = get_wikipedia_page_thumbnail_url(results[number-1])
-        print("({})".format(url))
+        name = results[number-1]
+        url = get_wikipedia_page_thumbnail_url(name)
+        print("\n({})".format(url))
+        save_path = download_image_from_url(url, name)
+        return save_path
+
     except Exception as e:
-        print(f"Error: Unable to find image for the given name: {e}")
-        return None, None
-    return url
+        print("Error: Unable to find image for the given name: {}\n".format(name))
+        return None
+
     
 def convert_image_to_cartoon(image_path):
     """
     Converts an image to a cartoon given the image_path.
     """
-    pass
-    # TODO (and remove the pass statement above)
+    image = cv2.imread(image_path)
 
-
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurImage = cv2.medianBlur(image, 1)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    color = cv2.bilateralFilter(image, 9, 200, 200)
+    cartoon = cv2.bitwise_and(color, color, mask = edges)
+    cv2.imwrite(image_path, cartoon)
+    print("Your image is called {}".format(image_path))
+    
+    return image_path
     
 if __name__ == "__main__":
-    prompt_for_image()
-    # TODO (and remove the pass statement above)
+
+    found_image = None
+    while found_image == None:
+        found_image = prompt_for_image()
+
+    convert_image_to_cartoon(found_image)
 
